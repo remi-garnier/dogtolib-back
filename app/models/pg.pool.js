2b9a7735abc3243e493f11aee19ba690f17dca29
remi-garnier <remi.garnier@proton.me>
2023-05-09 21:26:40 +0200

    feat(datamappers): Add sql queries logging

--- app/models/pg.pool.js
+++ app/models/pg.pool.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const debug = require('debug')('SQL:logger');
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -8,4 +9,12 @@ const pool = new Pool({
   database: process.env.PG_DATABASE,
 });
 
-module.exports = pool;
+module.exports = {
+  originalPool: pool,
+
+  async query(...params) {
+    debug(...params);
+
+    return this.originalPool.query(...params);
+  },
+};
