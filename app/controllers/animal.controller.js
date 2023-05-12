ea14b4a9f6662d78e4bc9e361e98fa55973e3522
remi-garnier <remi.garnier@proton.me>
2023-05-12 15:44:45 +0200

    feat(animal.controller) Implement 'updateAnimal' and 'deleteAnimal' methods

--- app/controllers/animal.controller.js
+++ app/controllers/animal.controller.js
@@ -5,12 +5,22 @@ const animalController = {
     const animals = await animal.findAllByOwnerId(req.userId);
     return res.json({ animals });
   },
+
+  /**
+   * @summary Créer un animal pour l'utilisateur connecté
+   * @returns 201 l'animal créé
+   */
   async createAnimal(req, res) {
     const account_id = req.userId;
     const newAnimal = await animal.create({ account_id, ...req.body });
     return res.status(201).json({ newAnimal });
   },
 
+  /**
+   *
+   * @ summary Renvoi les infos d'un animal donné
+   * @returns {Animal} 200 - Animal
+   */
   async getAnimal(req, res) {
     const { animalId } = req.params;
     const animalData = await animal.findByPk(animalId);
@@ -26,12 +36,51 @@ const animalController = {
     return res.json({ animalData });
   },
 
+  /**
+   * @summary Met à jour un animal donné
+   * @returns {Animal} 200 - Animal mis à jour
+   */
   async updateAnimal(req, res) {
-    res.json({ response: 'update animal' });
+    const { animalId } = req.params;
+    const toUpdate = await animal.findByPk(animalId);
+
+    // vérifier que l'animal existe
+    if (!toUpdate) {
+      return res.status(404).json({ error: 'Animal not found' });
+    }
+
+    // vérifier que l'utilisateur est propriétaire de l'animal
+    if (toUpdate.account_id !== req.userId) {
+      return res.status(403).json({ error: 'Forbidden' });
+    }
+    // Mettre à jour l'animal
+    const updatedAnimal = await animal.update({ id: animalId, ...req.body });
+
+    // Renvoyer 200 avec les nouvelles données
+    return res.json({ response: updatedAnimal });
   },
 
+  /**
+   * @summary Supprime un animal donné
+   */
   async deleteAnimal(req, res) {
-    res.json({ response: 'delete animal' });
+    const { animalId } = req.params;
+    const toDelete = await animal.findByPk(animalId);
+
+    // vérifier que l'animal existe
+    if (!toDelete) {
+      return res.status(404).json({ error: 'Animal not found' });
+    }
+
+    // vérifier que l'utilisateur est propriétaire de l'animal
+    if (toDelete.account_id !== req.userId) {
+      return res.status(403).json({ error: 'Forbidden' });
+    }
+
+    // Supprimer l'animal
+    await animal.delete(animalId);
+
+    return res.status(204).end();
   },
 
 };
