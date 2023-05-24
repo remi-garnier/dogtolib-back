d5bc755e22476ae8c80075970916b5d34a59f43b
Etudiant O'clock <franck.auloni@gmail.com>
2023-05-24 17:28:37 +0200

    Feat(mail): Configure scheduled mail for upcoming reminders
    Work in progress, not functionnal

---
+++ app/services/mail.service.js
@@ -0,0 +1,25 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp-mail.outlook.com',
+  secureConnection: false,
+  port: 587, // 25,
+  tls: {
+    ciphers: 'SSLv3',
+  },
+  auth: {
+    user: 'oclockdogtolib@outlook.com',
+    pass: 'dogtolib!1',
+  },
+});
+
+module.exports = {
+  sendMail: async (recipient, title, content, datetime) => {
+    const info = await transporter.sendMail({
+      from: '"Dogtolib" <oclockdogtolib@outlook.com>', // sender address
+      to: `${recipient}`, // list of receivers
+      subject: `Dogtolib: rappel ${title}`, // Subject line
+      text: ` Rappel: ${title} : ${content}  le ${datetime}`, // plain text body
+    });
+  },
+};
--- app/services/schedule.service.js
+++ app/services/schedule.service.js
@@ -1,11 +1,20 @@
 const schedule = require('node-schedule');
 const debug = require('debug')('app:schedule');
+const mailer = require('./mail.service');
 const { reminder } = require('../models/index.datamapper');
 
 async function printNextReminders() {
-  const reminders = await reminder.findUpcomingReminders(1);
+  const reminders = await reminder.findUpcomingReminders(2);
+  const promises = [];
   reminders.forEach((rem) => {
     debug(`Next reminder: ${rem.datetime}${rem.title}${rem.email}`);
+    promises.push(mailer.sendMail(rem.email, rem.title, rem.label, rem.datetime));
+  });
+
+  Promise.all(promises).then(() => {
+    debug('All reminders sent');
+  }).catch((error) => {
+    debug(error);
   });
 }
 
